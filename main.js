8f3c0dff8fa453e5f455852ccd0d7b656c189b66
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-07-16 19:39:14 +0300

    start js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-'use strict'
+"use strict";
+const randomNumber = Math.round(Math.random() * 100);
+
+const input = document.querySelector(".search-input__input");
+input.addEventListener("blur", () => {
+  const userNumber = input.target.value;
+  const massege = document.querySelector(".random-number__massege");
+  if (randomNumber == Number(userNumber)) {
+    massege.innerHTML("");
+  }
+});
