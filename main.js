eeee77d979213b2bfa8e46ee5acefdb6299f021b
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-07-17 13:45:47 +0300

    start js for stand calc

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-'use strict'
+"use strict";
+
+const standCalcFirstNum = document.querySelector("#standart-calc-first-num");
+const standCalcSecondNum = document.querySelector("#standart-calc-second-num");
+const standCalcResult = document.querySelector(".standart-calc__result");
+const standCalcOperations = document.querySelectorAll(
+  ".standart-calc__operator"
+);
+for (let elem of standCalcOperations) {
+  let result;
+  elem.addEventListener("click", () => {
+    let result = "error";
+    switch (elem.textContent) {
+      case "+":
+        result = standCalcFirstNum + standCalcSecondNum;
+      case "-":
+        result = standCalcFirstNum + standCalcSecondNum;
+      case "+":
+        result = standCalcFirstNum + standCalcSecondNum;
+      case "+":
+        result = standCalcFirstNum + standCalcSecondNum;
+      default:
+        result = "error";
+    }
+  });
+}
