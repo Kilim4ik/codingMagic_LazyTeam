7d3dfa089f8b0aeaa0b157c214696655e02a9083
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-07-18 18:21:41 +0300

    add main logic to football game

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-'use strict'
+"use strict";
+
+const footballField = document.querySelector(".football-field");
+const footballBall = document.querySelector(".football-ball");
+
+footballField.addEventListener("mousedown", (e) => {
+  let x = e.offsetX - footballBall.width / 2;
+  let y = e.offsetY - footballBall.height / 2;
+  footballBall.style.left = `${x}px`;
+  footballBall.style.top = `${y}px`;
+});
