4d5620e5d9c4b417711c5f3d9304e67dd6588ecf
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-07-18 23:41:55 +0300

    football

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ footballField.addEventListener("mousedown", (e) => {
   let x, y;
   console.log(e.offsetX, e.offsetY);
   if (e.offsetX < 25) x = 25 - footballBall.width / 2;
+  else if (footballField.offsetWidth - footballBall.width / 2 < e.offsetX)
+    x = footballField.offsetWidth - footballBall.width;
+  else x = e.offsetX - footballBall.width / 2;
+
   if (e.offsetY < 25) y = 25 - footballBall.height / 2;
-  if (e.offsetX >= footballField.offsetWidth)
-//   else e.
-    x = footballField.offsetWidth - footballBall.width / 2;
-  if (e.offsetY >= footballField.offsetWidth)
-    y = footballField.offsetWidth - footballBall.height / 2;
+  else if (footballField.offsetHeight - footballBall.height / 2 < e.offsetY)
+    y = footballField.offsetHeight - footballBall.height;
+  else y = e.offsetY - footballBall.height / 2;
 
   footballBall.style.left = `${x}px`;
   footballBall.style.top = `${y}px`;
-  console.log(x, y);
 });
-console.log(footballField.offsetWidth);
