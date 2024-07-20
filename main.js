f6c312135a4eca55c04caced342c0b15a8e15334
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-07-20 23:35:16 +0300

    add time calculator

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-'use strict'
+"use strict";
+
+const timeCalcInput = document.querySelector("#time-calc-input");
+const timeCalcButton = document.querySelector("#time-calc-button");
+const timeCalcResult = document.querySelector("#time-calc-result");
+
+timeCalcButton.addEventListener("click", () => showTimeCalcResult());
+
+function showTimeCalcResult() {
+  timeCalcResult.innerHTML = calculateTimeCalc();
+}
+function calculateTimeCalc() {
+  const time = timeCalcInput.value;
+  let result = ``;
+  if (time >= 1440) result += `${Math.floor(time / 60 / 24)}дн. `;
+  if (time >= 60) result += `${Math.floor((time / 60) % 24)}:`;
+  result += `${time % 60} `;
+  return result;
+}
