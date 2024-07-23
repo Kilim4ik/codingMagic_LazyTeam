e801f061cd9c8c81a096d60e839fb9e7509d7fb7
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-07-23 19:06:29 +0300

    scienties start

--- main.js
+++ main.js
@@ -1,1 +1,113 @@
-'use strict'
+"use strict";
+
+const scientists = [
+  {
+    name: "Albert",
+    surname: "Einstein",
+    born: 1879,
+    dead: 1955,
+    id: 1,
+  },
+  {
+    name: "Isaac",
+    surname: "Newton",
+    born: 1643,
+    dead: 1727,
+    id: 2,
+  },
+  {
+    name: "Galileo",
+    surname: "Galilei",
+    born: 1564,
+    dead: 1642,
+    id: 3,
+  },
+  {
+    name: "Marie",
+    surname: "Curie",
+    born: 1867,
+    dead: 1934,
+    id: 4,
+  },
+  {
+    name: "Johannes",
+    surname: "Kepler",
+    born: 1571,
+    dead: 1630,
+    id: 5,
+  },
+  {
+    name: "Nicolaus",
+    surname: "Copernicus",
+    born: 1473,
+    dead: 1543,
+    id: 6,
+  },
+  {
+    name: "Max",
+    surname: "Planck",
+    born: 1858,
+    dead: 1947,
+    id: 7,
+  },
+  {
+    name: "Katherine",
+    surname: "Blodgett",
+    born: 1898,
+    dead: 1979,
+    id: 8,
+  },
+  {
+    name: "Ada",
+    surname: "Lovelace",
+    born: 1815,
+    dead: 1852,
+    id: 9,
+  },
+  {
+    name: "Sarah E.",
+    surname: "Goode",
+    born: 1855,
+    dead: 1905,
+    id: 10,
+  },
+  {
+    name: "Lise",
+    surname: "Meitner",
+    born: 1878,
+    dead: 1968,
+    id: 11,
+  },
+  {
+    name: "Hanna",
+    surname: "Hammarström",
+    born: 1829,
+    dead: 1909,
+    id: 12,
+  },
+];
+let scientistsCode = "";
+const scientistsNewCodes = () =>
+  scientists.forEach(({ name: scientistsName, surname, born, dead }) => {
+    scientistsCode += `<div class="scientist"><p> ${scientistsName} ${surname} , ${born}-${dead} </p></div>`;
+  });
+scientistsNewCodes();
+document.querySelector(".scientists-card-container").innerHTML = scientistsCode;
+const scientistsVariant = document.querySelectorAll(".scientists-variant");
+scientistsVariant.forEach((button) => {
+  button.addEventListener("click", () => {
+    const text = button.innerHTML;
+    scientistsCode = "";
+    const txt = "Які вчені народилися в 19 ст.";
+    console.log(text == txt);
+    console.log(text);
+    console.log(txt);
+    switch (text) {
+      case "Які вчені народилися в 19 ст.":
+        const arr = scientists.filter(({ born }) => born > 1800 && born < 1900);
+        alert(arr);
+      default:
+        return;
+    }
+  });
+});
