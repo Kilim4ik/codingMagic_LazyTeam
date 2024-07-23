d38ca482863f1d1b6a8eda88053fa5cd62ce8ed7
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-07-23 19:22:26 +0300

    add halfOfBAll

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ const footballBall = document.querySelector(".football-ball");
 
 footballField.addEventListener("mousedown", (e) => {
   let x, y;
-  console.log(e.offsetX, e.offsetY);
-  if (e.offsetX < 25) x = 25 - footballBall.width / 2;
-  else if (footballField.offsetWidth - footballBall.width / 2 < e.offsetX)
+  const halfOfBall = footballBall.width / 2;
+  if (e.offsetX < halfOfBall) x = 0;
+  else if (footballField.offsetWidth - halfOfBall < e.offsetX)
     x = footballField.offsetWidth - footballBall.width;
-  else x = e.offsetX - footballBall.width / 2;
+  else x = e.offsetX - halfOfBall;
 
-  if (e.offsetY < 25) y = 25 - footballBall.height / 2;
-  else if (footballField.offsetHeight - footballBall.height / 2 < e.offsetY)
+  if (e.offsetY < halfOfBall) y = 0;
+  else if (footballField.offsetHeight - halfOfBall < e.offsetY)
     y = footballField.offsetHeight - footballBall.height;
-  else y = e.offsetY - footballBall.height / 2;
+  else y = e.offsetY - halfOfBall;
 
   footballBall.style.left = `${x}px`;
   footballBall.style.top = `${y}px`;
