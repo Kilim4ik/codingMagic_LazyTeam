ab7919a764becf29101fe69fdda770b047e059ec
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-07-24 13:53:33 +0300

    fix border

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ footballField.addEventListener("mousedown", (e) => {
   const halfOfBall = footballBall.width / 2;
   if (e.offsetX < halfOfBall) x = 0;
   else if (footballField.offsetWidth - halfOfBall < e.offsetX)
-    x = footballField.offsetWidth - footballBall.width;
+    x = footballField.offsetWidth - footballBall.width - 2;
   else x = e.offsetX - halfOfBall;
 
   if (e.offsetY < halfOfBall) y = 0;
   else if (footballField.offsetHeight - halfOfBall < e.offsetY)
-    y = footballField.offsetHeight - footballBall.height;
+    y = footballField.offsetHeight - footballBall.height - 2;
   else y = e.offsetY - halfOfBall;
 
   footballBall.style.left = `${x}px`;
